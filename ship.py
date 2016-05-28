a9729ffdb487f339bf04d2ba27ec89c9cd890d48
TurBoss <j.l.toledano.l@gmail.com>
2016-05-29 00:53:29 +0200

    Welcome back PID

--- ship.py
+++ ship.py
@@ -4,6 +4,8 @@
 
 # Ship
 
+from pid import PID
+
 from direct.actor.Actor import Actor
 
 class Ship:
@@ -11,8 +13,12 @@ class Ship:
 
         self.game = game
 
+        self.x_pid = PID(3.0, 5.0, 1.0)
+        self.z_pid = PID(3.0, 5.0, 1.0)
+
         self.model = Actor("data/ship.egg")
-        self.model.setPos(0, 50, 0)
+
+        self.model.setPos(0, 30, 0)
         self.model.setHpr(0, 0, 0)
 
 
@@ -26,8 +32,24 @@ class Ship:
             mpos = self.game.mouseWatcherNode.getMouse()
 
             x = mpos.getX() * 20
-            y = mpos.getY() * 20
+            z = mpos.getY() * 20
 
-            self.model.setPos(x, self.model.getY(), y)
+            self.x_pid.setPoint(x)
+            self.z_pid.setPoint(z)
+
+            pid_x = self.x_pid.update(self.model.getX())
+            pid_z = self.z_pid.update(self.model.getZ())
+
+            self.vx = pid_x
+            self.vz = pid_z
+
+            self.vx = min(10, self.vx)
+            self.vx = max(-10, self.vx)
+
+            self.vz = min(10, self.vz)
+            self.vz = max(-10, self.vz)
+
+            self.model.setX(self.model, self.vx * dt)
+            self.model.setZ(self.model, self.vz * dt)
 
         return task.cont
